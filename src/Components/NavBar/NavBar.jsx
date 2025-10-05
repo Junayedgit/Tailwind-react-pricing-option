e239ba94c0e187967d9b4395ac120c7f75a9448b
junayedgit <ahmedjonayed802@gmail.com>
2025-10-05 22:35:10 +0600

    update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { use, useState } from "react";
 import Link from "./Link";
+import { Menu, X } from "lucide-react";
+
 const navigationData = [
   { id: 1, name: "Home", path: "/" },
   { id: 2, name: "About", path: "/about" },
@@ -7,15 +9,26 @@ const navigationData = [
   { id: 4, name: "Projects", path: "/projects" },
   { id: 5, name: "Contact", path: "/contact" },
 ];
+const Links = navigationData.map((route) => (
+  <Link route={route} key={route.id}></Link>
+));
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <nav>
-      <ul className="flex gap-4 justify-center">
-        {navigationData.map((route) => (
-          <Link route={route} key={route.id}></Link>
-        ))}
-      </ul>
+    <nav className="flex justify-between items-center p-4">
+      <span className="flex gap-4" onClick={() => setOpen(!open)}>
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+        )}
+        <ul className="md:hidden">{Links}</ul>
+
+        <h3>My Navbar</h3>
+      </span>
+      <ul className="md:flex gap-4 hidden">{Links}</ul>
+      <button>Sign In</button>
     </nav>
 
     // <nav>
