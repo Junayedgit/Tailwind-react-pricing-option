dd31bffc6aae49764167b8938ab8f3528cbb0bc6
junayedgit <ahmedjonayed802@gmail.com>
2025-10-06 21:16:30 +0600

    navbar done

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import Link from "./Link";
 import { Menu, X } from "lucide-react";
 
@@ -16,19 +16,25 @@ const Links = navigationData.map((route) => (
 const NavBar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="flex justify-between items-center p-4">
+    <nav className="flex justify-between items-center p-4 ">
       <span className="flex gap-4" onClick={() => setOpen(!open)}>
         {open ? (
           <X className="md:hidden"></X>
         ) : (
           <Menu className="md:hidden"></Menu>
         )}
-        <ul className="md:hidden">{Links}</ul>
+        <ul
+          className={`md:hidden absolute ${
+            open ? "top-10" : "-top-48"
+          } duration-1000 z-10 bg-white p-4 rounded-lg shadow-lg text-black`}
+        >
+          {Links}
+        </ul>
 
         <h3>My Navbar</h3>
       </span>
       <ul className="md:flex gap-4 hidden">{Links}</ul>
-      <button>Sign In</button>
+      <button className="btn">Sign In</button>
     </nav>
 
     // <nav>
